cca019c006e563aff116d4279cde048b7008dcd4
ARJUN <pachangedinanath@gmail.com>
2025-12-10 01:35:08 +0530

    Change progress bar symbols and update URLs

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -33,25 +33,25 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "♡—————————"
+        bar = "🐾—————————"
     elif 10 < umm < 20:
-        bar = "—♡————————"
+        bar = "—🐾————————"
     elif 20 <= umm < 30:
-        bar = "——♡———————"
+        bar = "——🐾———————"
     elif 30 <= umm < 40:
-        bar = "———♡——————"
+        bar = "———🐾——————"
     elif 40 <= umm < 50:
-        bar = "————♡—————"
+        bar = "————🐾—————"
     elif 50 <= umm < 60:
-        bar = "—————♡————"
+        bar = "—————🐾————"
     elif 60 <= umm < 70:
-        bar = "——————♡———"
+        bar = "——————🐾———"
     elif 70 <= umm < 80:
-        bar = "———————♡——"
+        bar = "———————🐾——"
     elif 80 <= umm < 95:
-        bar = "————————♡—"
+        bar = "————————🐾—"
     else:
-        bar = "—————————♡"
+        bar = "—————————🎉"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
@@ -68,10 +68,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/PURVI_SUPPORT",
+                text="𝖮ᴡɴᴇʀ 🥀", url="https://t.me/departed_fr",
             ),
             InlineKeyboardButton(
-                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/PURVI_UPDATES",
+                text="𝖲ᴜᴘᴘᴏʀᴛ 🥀", url="https://t.me/dark_musicsupport",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝖮𝗐𝗇𝖾𝗋🥀", url="https://t.me/PURVI_SUPPORT",
+                text="𝖮𝗐𝗇𝖾𝗋🥀", url="https://t.me/onigirisannn",
             ),
             InlineKeyboardButton(
-                text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍🥀", url="https://t.me/PURVI_UPDATES",
+                text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍🥀", url="https://t.me/dark_musictm",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
